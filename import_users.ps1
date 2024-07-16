e3eea8462f0087c4bb85aba411e73370879c550b
jpmpontes <65799519+jpmpontes@users.noreply.github.com>
2024-07-16 14:54:53 -0300

    Update import_users.ps1

--- import_users.ps1
+++ import_users.ps1
@@ -1,5 +1,5 @@
-﻿$Path="OU=Exchange,OU=BBRJ,DC=bbrj,DC=com,DC=br"
-Import-Csv “C:\BBRJ\exchange_tijuca.csv” -Delimiter ","| ForEach-Object {
+﻿$Path="<OU para aonde irao os usuarios>"
+Import-Csv “<caminho do arquivo de importacao dos usuarios>” -Delimiter ","| ForEach-Object {
 New-ADUser -Name $_.Name -GivenName $_."GivenName" -Surname $_."Surname" -DisplayName $_."DisplayName" -Path $Path -SamAccountName $_."SamAccountName" -UserPrincipalName $_."UserPrincipalName" -title $_."title" -Company $_."Company" -AccountPassword (ConvertTo-SecureString Teste@123. -AsPlainText -Force) -ChangePasswordAtLogon $false -Enabled $true -EmailAddress $_."EmailAddress"
 }
 
