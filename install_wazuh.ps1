6dad0763a3af8f9ab68d27e0a4f0b8e1a5a9f4b6
jpmpontes <65799519+jpmpontes@users.noreply.github.com>
2024-08-01 14:37:22 -0300

    Update install_wazuh.ps1

--- install_wazuh.ps1
+++ install_wazuh.ps1
@@ -1,4 +1,4 @@
-﻿$nome=hostname
+﻿$nome = hostname
 Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.8.0-1.msi -OutFile ${env.tmp}\wazuh-agent; msiexec.exe /i ${env.tmp}\wazuh-agent /q WAZUH_MANAGER='<endereco ip do servior do wazuh>' WAZUH_AGENT_NAME=$nome
 start-sleep -Seconds 30
 start-service -Name "Wazuh"
